ca1d1e38d6e74a9f48c57ba6b55ab94b2fef3d65
rubyhirsch <71003519+rubyhirsch@users.noreply.github.com>
2025-02-18 11:09:31 -0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Ruby Hirsch 
+// Date: 16 February 2025
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
